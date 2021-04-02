9647baf8eac7c8588fe87af21bd5058cc5fca026
Nikolay <nikolay.topalov@gmail.com>
2021-04-02 19:32:39 +0300

    init

---
+++ src/components/SkillsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>
+      My jouner as full-stack developer begins.<br>
+      Continue scrolling down to see what i can do!
+    </p>
+    <h3>Front-End</h3>
+    <h3>Back-End</h3>
+    <h3>Databases</h3>
+    <h3>Serverless and Clouds</h3>
+    <h3>Other Technologies</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkillsList',
+}
+</script>
+
+<style scoped lang="stylus">
+h3
+  margin 40px 0 0
+
+a
+  color #42b983
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SkillsList />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SkillsList from '@/components/SkillsList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SkillsList
   }
 }
 </script>
